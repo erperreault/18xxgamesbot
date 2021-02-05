d2335558651b21fcc2382c6c1692fa3a530d12f9
erperreault <erperreault@gmail.com>
2021-02-05 13:00:08 -0500

    newgame formatting

--- server.py
+++ server.py
@@ -6,14 +6,14 @@
 import discord, json, os
 
 client = discord.Client()
-busy = []
+server = discord.Member().guild
+
+help_command = '!help'
 
 @client.event
 async def on_ready():
-    '''Status report!'''
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {client.user} on {server}')
 
-# Herein lie message events.
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -21,13 +21,13 @@ async def on_message(message):
 
     elif message.author not in busy and str(message.channel.type) == 'private':
         if not message.content.startswith('!'):
-            await message.channel.send('Hello! Say "!help" if you need help.')
+            await message.channel.send(f'Hello! Say "{help_command}" if you need help.')
 
-        elif message.content.startswith('!help'):
+        elif message.content.startswith(help_command):
             await bot_help(message)
 
-        elif message.content.startswith('!newgame'):
-            await newgame(message)
+        else:
+            await message.channel.send(f'Sorry, I don\'t know that command. Say "{help_command}" for the commands I do know.')
 
 
 ###
@@ -36,70 +36,9 @@ async def on_message(message):
 async def bot_help(message):
     '''List of all accepted commands.'''
 
-    await message.channel.send('''Here are the commands I know:
+    await message.channel.send(f'''Here are the commands I know:
             hello - I'll say hello back :)
-            !help - See this dialogue.
-            !newgame - Schedule an upcoming game session.
-            !schedule - List all currently scheduled games.''')
-
-async def newgame(message):
-    '''Enter a dialogue to schedule a game session.
-    Mark user as "busy" until this is done or cancelled.'''
-    
-    global busy
-    channel = message.channel
-    author = message.author
-    newgame_dict = {}
-
-    def identity_check(message):
-        return message.channel == channel and message.author == author and message.author in busy
-
-    async def cancel(message):
-        busy.remove(author)
-        await message.channel.send('Cancelling game creation.')
-        await newgame.cancel()
-
-    async def savegame(entry):
-        '''Imports .log.json. 
-        Start file in format if empty. 
-        Add new game to the lineup with a unique ID.'''
-        with open('.log.json', 'r') as log:
-            try:
-                schedule = json.load(log)
-            except json.decoder.JSONDecodeError:
-                schedule = {"key" : 0}
-
-        newkey = schedule['key'] + 1
-        schedule['key'] = newkey
-        schedule[newkey] = entry
-
-        with open('.log.json', 'w+') as log:
-            json.dump(schedule, log)
-
-    async def check_for_cancel():
-        '''First check that message is in the same thread, then check for '!cancel'.'''
-        x = await client.wait_for('message', check=identity_check)
-        if x.content == '!cancel':
-            await cancel(message)
-        return x.content
-
-    busy.append(author)
-
-    await channel.send('Great, which game? Say !cancel to cancel.')
-        
-    title = await check_for_cancel()
-    newgame_dict['title'] = title
-    await channel.send(f'Scheduling {title}. On what date? Please format like so: 24jan, or 15mar.')
-
-    date = await check_for_cancel()
-    newgame_dict['date'] = date
-
-    await savegame(newgame_dict)
-
-    await channel.send(f"Scheduled {title} on {date}. I'll make a channel for it. Thanks!")
-    print(f'User {author} scheduled {title} for {date}.') 
-
-    busy.remove(author)
+            {help_command} - See this dialogue.''')
 
 
 ###
