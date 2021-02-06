e5dd8878c23bcf43aaef693f488dbbc4a01ef749
erperreault <erperreault@gmail.com>
2021-02-06 13:52:14 -0500

    make @mentions when updates found

--- server.py
+++ server.py
@@ -7,14 +7,15 @@ client = discord.Client(intents = intents)
 help_command = '!help'
 track_command = '!track'
 startup_command = '!startup'
+sync_command = '!sync'
 tracked_games_fp = '.tracked_games.json'
+player_log_fp = '.player_log.json'
 
 ###
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}.')
-    print(f'{client.get_channel(795114528118669334)}')
     await auto_checker()
 
 @client.event
@@ -26,6 +27,9 @@ async def on_message(message):
 
         if message.content.startswith(track_command):
             await track_game(message)
+
+        if message.content.startswith(sync_command):
+            await sync_player_ids(message)
 
     elif str(message.channel.type) == 'private' and message.author != client.user:
 
@@ -56,6 +60,20 @@ def get_acting_id(game: str) -> str:
     except:
         print('no acting player found')
         return 'none or game over'
+
+
+def get_player_name_from_id(id, game):
+    for player in game['players']:
+        if player['id'] == int(id):
+            return player['name']
+    return id
+
+
+def get_discord_mention_from_player_name(name, log):
+    try:
+        return f'<@{log[name]}>'
+    except:
+        return name
 
 
 async def bot_help(message):
@@ -116,16 +134,24 @@ async def check_all_games():
             log = json.load(log_file)
     except:
         log = {'channels': {}, 'games': {}}
+    try:
+        with open(player_log_fp, 'r') as player_log_file:
+            player_log = json.load(player_log_file)
+    except:
+        player_log = {}
 
     for channel in log['channels']:
         for game_id in log['channels'][channel]:
             game = get_game_data(game_id)
             true_acting = get_acting_id(game)
+            acting_discord_name = get_player_name_from_id(true_acting, game)
+            discord_mention = get_discord_mention_from_player_name(acting_discord_name, player_log)
             if log['games'][game_id]['acting'] != true_acting:
                 print(f'Updating acting player for {game_id}: {true_acting}')
                 log['games'][game_id]['acting'] = true_acting
                 target = client.get_channel(int(channel))
-                await target.send(f'{true_acting}')
+                await target.send(f'''{discord_mention} - your turn! 
+https://18xx.games/game/{game_id}''')
 
     with open(tracked_games_fp, 'w') as log_file:
         json.dump(log, log_file)
@@ -135,6 +161,25 @@ async def auto_checker():
     while True:
         await check_all_games()
         await asyncio.sleep(10)
+
+async def sync_player_ids(message):
+    web_id = message.content.split(' ')[1]
+    discord_id = str(message.author.id)
+
+    try:
+        with open(player_log_fp, 'r') as player_log:
+            log = json.load(log_file)
+    except:
+        log = {}
+
+    log[web_id] = discord_id
+    log[discord_id] = web_id
+
+    with open(player_log_fp, 'w') as player_log:
+        json.dump(log, player_log)
+
+    await message.channel.send(f'Successfully synced {message.author} as {web_id}.')
+    print(f'Synced {discord_id} as {web_id}')
 
 ###
 
