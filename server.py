28431e87a193eddbabc3c8a32a08b0d922e37a43
erperreault <erperreault@gmail.com>
2021-02-05 21:45:00 -0500

    store game+channel ids including acting player

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import os, json, urllib.request, discord, re
 client = discord.Client()
 help_command = '!help'
 track_command = '!track'
+tracked_games_fp = '.tracked_games.json'
 
 ###
 
@@ -36,6 +37,19 @@ async def on_message(message):
                 f'Sorry, I don\'t know that command. Say "{help_command}" for the commands I do know.')
 
 ###
+            
+def get_game_data(id: str) -> str:
+    with urllib.request.urlopen(f'https://18xx.games/api/game/{id}') as response:
+        return json.loads(response.read())
+
+
+def get_all_player_ids(game: str) -> list:
+    return [player['id'] for player in game['players']]
+
+
+def get_acting_id(game: str) -> str:
+    return str(game['acting'][0])
+
 
 async def bot_help(message):
     '''List of all accepted commands.'''
@@ -50,36 +64,41 @@ async def track_game(message):
     '''Associate a linked game ID with the channel it's given in.'''
 
     id_target = re.compile(r'\d\d\d+')
-    game_id = id_target.findall(message.content)
-    channel_id = message.channel.id
-    if len(game_id) == 1:
-        await message.channel.send(f'Tracking game ID {game_id[0]} in this channel ({message.channel}).')
-        print(f'Tracking {game_id[0]} on channel {channel_id}.')
-    else:
+    id_results = id_target.findall(message.content)
+    channel_id = str(message.channel.id)
+
+    if len(id_results) != 1:
         await message.channel.send(
-            """Sorry, I couldn't figure out the game ID :japanese_ogre:
+"""Sorry, I couldn't figure out the game ID :japanese_ogre:
 Your command should look something like this: 
-    !track https://18xx.games/game/25902.""")
-            
+    !track https://18xx.games/game/25902
+    or
+    !track 25902""")
+    else:
+        game_id = id_results[0]
+        game_data = get_game_data(game_id)
 
-def get_game_data(id: str) -> json:
-    with urllib.request.urlopen(f'https://18xx.games/api/game/{id}') as response:
-        return json.loads(response.read())
+        try:
+            with open(tracked_games_fp, 'r') as log_file:
+                log = json.load(log_file)
+        except:
+            log = {'channels': {}, 'games': {}}
 
+        try:
+            if game_id not in log['channels'][channel_id]:
+                log['channels'][channel_id].append(game_id)
+        except KeyError:
+            log['channels'][channel_id] = [game_id]
 
-def get_all_player_ids(game: json) -> list:
-    return [player['id'] for player in game['players']]
+        if game_id not in log['games'].keys():
+            log['games'][game_id] = {}
+        log['games'][game_id]['acting'] = get_acting_id(game_data)
 
+        with open(tracked_games_fp, 'w') as log_file:
+            json.dump(log, log_file)
+        await message.channel.send(f'Tracking game ID {game_id} in this channel ({message.channel}).')
 
-def get_acting_id(game: json) -> int:
-    return game['acting'][0]
-
-# for example    
-'''
-my_game_id = '25902'
-game_data = get_game_data(my_game_id)
-print(get_acting_id(game_data))
-'''
+        print(f'Tracking {game_id} on channel {channel_id}.')
 
 ###
 
