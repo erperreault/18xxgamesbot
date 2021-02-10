dc8ddffcd20405abd39737756518fa0eeece514c
erperreault <erperreault@gmail.com>
2021-02-09 20:11:27 -0500

    make sql stuff more functional, needs cleaning up

--- sql_client.py
+++ sql_client.py
@@ -1,25 +1,50 @@
 import sqlite3
 
-def make_user_table(cursor: object):
-    cursor.execute('CREATE TABLE IF NOT EXISTS users (web_name text, web_id text unique, discord_id text unique)')
+def connect(db_fp):
+    return sqlite3.connect(db_fp)
 
-def insert_user(cursor: object, web_name: str, web_id: str, discord_id: str):
-    cursor.execute('INSERT INTO users VALUES (?, ?, ?)', (web_name, web_id, discord_id))
+def execute_query(connection, query, args=()):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query, args)
+        connection.commit()
+    except Exception as e:
+        print(f'{query} // Error: {e}')
 
-def select_user(cursor: object, discord_id: str) -> list:
-    return cursor.execute('SELECT * FROM users WHERE discord_id = ?', (discord_id,)).fetchone()
+def read_query(connection, query, args=()):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query, args)
+        return cursor.fetchall()
+    except Exception as e:
+        print(f'{query} // Error: {e}')
 
-def update_user(cursor: object, discord_id: str, new_name: str):
-    cursor.execute('UPDATE users SET web_name = ? WHERE web_name = ?', (new_name, discord_id))
+def make_user_table(connection: object):
+    execute_query(connection, 'CREATE TABLE IF NOT EXISTS users (web_name text, web_id text unique, discord_id text unique)')
 
-def make_games_table(cursor: object):
-    cursor.execute('CREATE TABLE IF NOT EXISTS games (game_id text, channel text, acting_player text)')
+def insert_user(connection: object, web_name: str, web_id: str, discord_id: str):
+    execute_query(connection, 'INSERT INTO users VALUES (?, ?, ?)', (web_name, web_id, discord_id))
 
-def insert_game(cursor: object, game_id: str, channel: str, acting_player: str):
-    cursor.execute('INSERT INTO games VALUES (?, ?, ?)', (game_id, channel, acting_player))
+def select_user(connection: object, discord_id: str) -> list:
+    return read_query(connection, 'SELECT * FROM users WHERE discord_id = ?', (discord_id,))
 
-def select_game(cursor: object, game_id: str) -> list:
-    return cursor.execute('SELECT * FROM games WHERE game_id = ?', (game_id,)).fetchone()
+def select_user_by_web_id(connection: object, web_id: str) -> list:
+    return read_query(connection, 'SELECT * FROM users WHERE web_id = ?', (web_id,))
 
-def update_acting_player(cursor: object, game_id, acting_player: str):
-    cursor.execute('UPDATE games SET acting_player = ? WHERE game_id = ?', (acting_player, game_id))
+def update_user(connection: object, discord_id: str, new_name: str):
+    execute_query(connection, 'UPDATE users SET web_name = ? WHERE discord_id = ?', (new_name, discord_id))
+
+def make_games_table(connection: object):
+    execute_query(connection, 'CREATE TABLE IF NOT EXISTS games (game_id text unique, channel text, acting_player text)')
+
+def insert_game(connection: object, game_id: str, channel: str, acting_player: str):
+    execute_query(connection, 'INSERT INTO games VALUES (?, ?, ?)', (game_id, channel, acting_player))
+
+def select_game(connection: object, game_id: str) -> list:
+    return read_query(connection, 'SELECT * FROM games WHERE game_id = ?', (game_id,))
+
+def update_acting_player(connection: object, game_id, acting_player: str):
+    execute_query(connection, 'UPDATE games SET acting_player = ? WHERE game_id = ?', (acting_player, game_id))
+
+def update_game_channel(connection: object, channel_id: str, game_id: str):
+    execute_query(connection, 'UPDATE games SET channel = ? WHERE game_id = ?', (channel_id, game_id))
---
+++ verbage.py
@@ -0,0 +1,15 @@
+def discord_mention(mention, game_id):
+    return f'''{mention} - your turn! 
+https://18xx.games/game/{game_id}'''
+
+game_id_error = '''Sorry, I couldn't figure out the game ID :japanese_ogre:
+Your command should look something like this: 
+    !track https://18xx.games/game/25902
+    or
+    !track 25902'''
+
+def help_message(help_command, track_command):
+    return f'''Here are the commands I know:
+            hello - I'll say hello back :)
+            {help_command} - See this dialogue.
+            {track_command} [game ID or link] - Track an async game for @mention notifications.'''
