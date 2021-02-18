f3510247737cf47dfb8a8a7020540ffeeec0956b
erperreault <erperreault@gmail.com>
2021-02-17 19:26:25 -0500

    postgres

--- sql_client.py
+++ sql_client.py
@@ -23,31 +23,31 @@ def make_user_table(connection: object):
     execute_query(connection, 'CREATE TABLE IF NOT EXISTS users (web_name text, web_id text unique, discord_id text unique)')
 
 def insert_user(connection: object, web_name: str, web_id: str, discord_id: str):
-    execute_query(connection, 'INSERT INTO users VALUES (?, ?, ?)', (web_name, web_id, discord_id))
+    execute_query(connection, 'INSERT INTO users VALUES (%s, %s, %s)', (web_name, web_id, discord_id))
 
 def select_user(connection: object, discord_id: str) -> list:
-    return read_query(connection, 'SELECT * FROM users WHERE discord_id = ?', (discord_id,))
+    return read_query(connection, 'SELECT * FROM users WHERE discord_id = %s', (discord_id,))
 
 def select_user_by_web_id(connection: object, web_id: str) -> list:
-    return read_query(connection, 'SELECT * FROM users WHERE web_id = ?', (web_id,))
+    return read_query(connection, 'SELECT * FROM users WHERE web_id = %s', (web_id,))
 
 def update_user(connection: object, discord_id: str, new_name: str):
-    execute_query(connection, 'UPDATE users SET web_name = ? WHERE discord_id = ?', (new_name, discord_id))
+    execute_query(connection, 'UPDATE users SET web_name = %s WHERE discord_id = %s', (new_name, discord_id))
 
 def make_games_table(connection: object):
     execute_query(connection, 'CREATE TABLE IF NOT EXISTS games (game_id text unique, channel text, acting_player text)')
 
 def insert_game(connection: object, game_id: str, channel: str, acting_player: str):
-    execute_query(connection, 'INSERT INTO games VALUES (?, ?, ?)', (game_id, channel, acting_player))
+    execute_query(connection, 'INSERT INTO games VALUES (%s, %s, %s)', (game_id, channel, acting_player))
 
 def select_game(connection: object, game_id: str) -> list:
-    return read_query(connection, 'SELECT * FROM games WHERE game_id = ?', (game_id,))
+    return read_query(connection, 'SELECT * FROM games WHERE game_id = %s', (game_id,))
 
 def update_acting_player(connection: object, game_id, acting_player: str):
-    execute_query(connection, 'UPDATE games SET acting_player = ? WHERE game_id = ?', (acting_player, game_id))
+    execute_query(connection, 'UPDATE games SET acting_player = %s WHERE game_id = %s', (acting_player, game_id))
 
 def update_game_channel(connection: object, channel_id: str, game_id: str):
-    execute_query(connection, 'UPDATE games SET channel = ? WHERE game_id = ?', (channel_id, game_id))
+    execute_query(connection, 'UPDATE games SET channel = %s WHERE game_id = %s', (channel_id, game_id))
 
 def delete_game(connection: object, game_id: str):
-    execute_query(connection, 'DELETE FROM games WHERE game_id = ?', (game_id,))
+    execute_query(connection, 'DELETE FROM games WHERE game_id = %s', (game_id,))
