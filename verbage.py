da3c17a0e1db33514bd3cfa543b58538153280b2
erperreault <erperreault@gmail.com>
2021-02-17 20:16:53 -0500

    improve help message

--- verbage.py
+++ verbage.py
@@ -11,10 +11,12 @@ Your command should look something like this:
 def help_message(help_command, track_command, sync_command):
     return f'''
     Here are the commands I know:
+    
         "hello" - I'll say hello back :)
         "{help_command}"" - See this dialogue.
         "{track_command} [game ID or link]" - Track an async game for @mention notifications.
         "{sync_command} [18xx.games username]" - Associate your website username with your current Discord account.
+
     All commands must be used in a public channel. 
     When tracking a game, do so in the associated channel.
     Before syncing your username, you must be in at least one tracked game.
